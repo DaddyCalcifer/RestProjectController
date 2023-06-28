40a3c41480ce55f9943bc3b7b4983f3de7ab03d4
Никита Черничкин <nikita.macarov@yandex.ru>
2023-06-28 14:00:50 +0300

    Добавьте файлы проекта.

---
+++ RestProjectController/Controllers/FlatsController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace RestProjectController.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FlatsController : ControllerBase
+    {
+        static MongoClient client = new MongoClient("mongodb://localhost:27017");
+        public string Index()
+        {
+            return "xd";
+        }
+        [HttpGet("all")]
+        public async Task<string> Get()
+        {
+            var db = client.GetDatabase("RestApp");
+            var collection = db.GetCollection<BsonDocument>("Flats");
+
+            var Flat = await collection.Find("{}").ToListAsync();
+            return Flat.ToJson();
+        }
+        [HttpGet("ByName/{name}")]
+        public async Task<string> GetByName(string name)
+        {
+            var db = client.GetDatabase("RestApp");
+            var collection = db.GetCollection<BsonDocument>("Flats");
+
+            var Flat = await collection.Find(new BsonDocument("Name", name)).ToListAsync();
+            return Flat.ToJson();
+        }
+        [HttpGet("BySleep/{count}")]
+        public async Task<string> GetBySP(string count)
+        {
+            var db = client.GetDatabase("RestApp");
+            var collection = db.GetCollection<BsonDocument>("Flats");
+
+            var Flat = await collection.Find(new BsonDocument("SleepPlaces", int.Parse(count))).ToListAsync();
+            return Flat.ToJson();
+        }
+        [HttpGet("ByFull/{val}")]
+        public async Task<string> GetByFull(string val)
+        {
+            var db = client.GetDatabase("RestApp");
+            var collection = db.GetCollection<BsonDocument>("Flats");
+
+            var Flat = await collection.Find(new BsonDocument("fullFlat", Convert.ToBoolean(val))).ToListAsync();
+            return Flat.ToJson();
+        }
+        [HttpGet("Add/{name}/{full}/{sleep}/{cost}")] //пока get-запрос для проверки и тестового заполнения бд, потом перепишу его в post
+        public async Task<string> AddKv(string name,string full, string sleep, string cost)
+        {
+            var db = client.GetDatabase("RestApp");
+            var collection = db.GetCollection<BsonDocument>("Flats");
+
+            if (await collection.CountDocumentsAsync(new BsonDocument { { "Name", name }, { "fullFlat", Convert.ToBoolean(full) }, { "SleepPlaces", int.Parse(sleep) }, { "Price", Convert.ToDecimal(cost) } }) == 0)
+            {
+                await collection.InsertOneAsync(new BsonDocument{ { "Name", name},{ "fullFlat",Convert.ToBoolean(full)},{ "SleepPlaces", int.Parse(sleep)}, {"Price", Convert.ToDecimal(cost) } });
+            }
+            var users = await collection.Find("{}").ToListAsync();
+            var Flat = await collection.Find(new BsonDocument { { "Name", name }, { "fullFlat", Convert.ToBoolean(full) }, { "SleepPlaces", int.Parse(sleep) }, { "Price", Convert.ToDecimal(cost) } }).ToListAsync();
+            return Flat.ToJson();
+        }
+    }
+}
---
+++ RestProjectController/Models/Flats.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace RestProjectController.Models
+{
+    public class Flats
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string Name { get; set; } = null!;
+        public bool fullFlat { get; set; } = false!;
+        public int SleepPlaces { get; set; } = 1!;
+        public decimal Price { get; set; }
+    }
+}
